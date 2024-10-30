d999c31fea843f1192475b86aec7b1fdb1c8c855
Jpnow06 <jp.nowacki2@student.uw.edu.pl>
2024-10-30 21:47:06 +0300

    JJaaas2

--- Intro_julia/homework.jl
+++ Intro_julia/homework.jl
@@ -20,3 +20,17 @@ x=[1, 2, 5]
  standard_deviation(x)
  n
 sum(x)
+#Problem 7
+data_homework = readdlm("data_analysis//datasets//dataset.csv", ',',Float64)
+data_homework
+earnings=(data_homework[:,1])
+education=(data_homework[:,2])
+hours_worked=(data_homework[:,3])
+plot_1 = scatter(education,earnings ; legend=false, color=:blue, markersize = 5, opacity=0.7)
+plot_2 = scatter(hours_worked,earnings ; legend=false, color=:blue, markersize = 5, opacity=0.7)
+xaxis!(plot_1, "education")
+yaxis!(plot_1, "earnings")
+xaxis!(plot_2, "hours_worked")
+yaxis!(plot_2, "earnings")
+cor(earnings,education)
+cor(earnings,hours_worked)
