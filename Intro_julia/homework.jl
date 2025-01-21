7c685f0e7f2074da6cb4199be6f4d8d0f35e2c2b
Jpnow06 <jp.nowacki2@student.uw.edu.pl>
2025-01-21 20:42:50 +0100

    j

--- Intro_julia/homework.jl
+++ Intro_julia/homework.jl
@@ -1,79 +1,62 @@
-using PrettyTables, Plots, LaTeXStrings, LinearAlgebra, NLsolve, Roots, Iterativesolvers, Calculus
+using Parameters, Interpolations, Plots, LinearAlgebra, QuantEcon, Distributions, Random
 
-# Homework 2
-
-2.
-function exact_solution(α,β)
-    x5=1
-    x4=x5
-    x3=x4
-    x2=x3
-    x1=x2-α-x4*(α-β)-x5*(β)
-    return [x1,x2,x3,x4,x5]
-
-    
-    
-end
-exact_solution(4,7)
-function backlslash_x(α,β) 
-A=[1 -1 0 α-β β ; 0 1 -1 0 0 ; 0 0 1 -1 0 ; 0 0 0 1 -1 ; 0 0 0 0 1]
-b=[α ; 0 ; 0 ; 0 ; 1]
-x = A\ b
-return x
-end
-backlslash_x(2,8) 
-
-function relative_residual(α,β)
-    A=[1 -1 0 α-β β ; 0 1 -1 0 0 ; 0 0 1 -1 0 ; 0 0 0 1 -1 ; 0 0 0 0 1]
-b=[α ; 0 ; 0 ; 0 ; 1]
-return norm(A*backlslash_x(α,β)-b)/norm(b)
-end
-relative_residual(4,5)
-
-function condition_numerator(α,β)
-    A=[1 -1 0 α-β β ; 0 1 -1 0 0 ; 0 0 1 -1 0 ; 0 0 0 1 -1 ; 0 0 0 0 1]
-b=[α ; 0 ; 0 ; 0 ; 1]
- norm_A=norm(A,2)
- inv_norm_A=norm(inv(A))
- return norm_A*inv_norm_A
-end
-
-condition_numerator(3,16)
+#Probem 1
 
 
+# Problem 2
+function mc_sample_path(P; init_x = 1, sample_size = 100)
+    @assert size(P)[1] == size(P)[2] # square required
+    N = size(P)[1] # number of states
+    dists = [Categorical(P[i, :]) for i in 1:N]
+    X = Vector{Int64}(undef, sample_size) # allocate memory
+    X[1] = init_x # set the initial state
 
-function task_1(f, x0; tol=1e-6, maxiter=1000, α=0.0)
-g(x) = f(x) + x
-
-x_values = Float64[x0]
-residuals = Float64[] 
-
-for iteration in 1:maxiter
-
-    x1 = (1 - α) * g(x0) + α*x0
-    push!(x_values, x1)
-
-    residual = abs(x1 - x0)
-    push!(residuals, residual)
-
-    if residual < tol
-        return (0, x1, f(x1), abs(x1 - g(x1)), x_values, residuals)
+    for t in 2:sample_size
+        previous_state_value = X[t-1]
+        P_Xt = dists[previous_state_value] # appropriate distribution  
+        X[t] = rand(P_Xt) # draw new value
     end
-
-    x0 = x1
+    return X
 end
+α = 0.1;   # finds job  
+β = 0.05;    # loses job
 
-return (1, NaN, NaN, NaN, x_values, residuals)
-end
+P = [1-α α; 
+     β 1-β]
+     ψ0 = [0.05 0.95]
+     periods = 100
+     sample_path_initU = mc_sample_path(P, init_x = 1, sample_size = periods);
+     sample_path_initE = mc_sample_path(P, init_x = 2, sample_size = periods);
+     
+     
+     plot(sample_path_initE, label = "start employed")
+     plot!(sample_path_initU, label = "start unemployed")
 
-function somefunction()
-f(x) = (x + 1)^(1/3) - x
+     # Poblem 2 mccall
+     function create_job_search_model(;
+        n=100, # wage grid size
+        w_min=10.0, # lowest wage
+        w_max=60.0, # highest wage
+        β=0.96, # discount factor
+        c=10.0, # unemployment compensation
+      p=0.01
+        )
+        wages = range(w_min, w_max, length=n)
 
-result = task_1(f, 1.0; tol=1e-6, maxiter=1000, α=0.0)
-println("Result: ", result)
-end
-
-somefunction()
-
-
-
+        return (; n, wages, β, c)
+    end
+    my_model=return create_job_search_model
+    π = ones(length(wages)) ./ length(wages)
+    V_u=zeros(n)
+    V_e=zeros(n)
+    V_u_new = [max(V_e[w], c + β * sum(V_u .* π )) for w in 1:n]
+    V_e_new = [wages[w] + β * ((1 - p) * V_e[w] + p * sum(V_u .* π)) for w in 1:n]
+    V_u .= V_u_new
+        V_e .= V_e_new
+        w=  minimum(wages[V_e .>= c + β * sum(V_u .* π)]) #reservation wage
+        reservation_wages = [create_job_search_model(p=p)[:reservation_wage] for p in p_values]
+        p_values = 0:0.05:1.0
+        plot(
+    p_values, w, xlabel="p", ylabel="Reservation Wage (w)", title="propability of unemployed", label="w*",
+    legend=:bottomright
+)
